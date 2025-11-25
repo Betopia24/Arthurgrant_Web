e74358895666cb669fbeb2312fbe9cf3278bfca1
engrnahiankhan <engrnahiankhan@gmail.com>
2025-11-25 12:09:10 +0600

    presentation 2 task complete

---
+++ redux/features/presentation/presentationApi.tsx
@@ -0,0 +1,21 @@
+import { baseApi } from "@/redux/api/baseApi";
+
+const presentationApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    getPowerWords: builder.query({
+      query: () => ({
+        url: "/presentation/power-words/get_power_words",
+        method: "GET",
+      }),
+    }),
+    getPowerWordsScore: builder.mutation({
+      query: (data) => ({
+        url: "/presentation/power-words/power_words",
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const {} = presentationApi;
